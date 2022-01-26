e2ec5af2706611405448a97ff8447199c153b719
indo deriano t halim <mde50526@gmail.com>
2022-01-27 02:01:20 +0700

    using older react-scripts version, router is set up

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css'
+import Home from './pages/Home'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        
+      </Route>
+    </Routes>
+  </BrowserRouter>
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import { Header, Grid, Card, Image, Icon } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import ironman from '../assets/iron-man.webp'
+
+export default function Home () {
+
+    return (
+        <div
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignContent: 'center',
+                padding: '0 20%',
+                overflow: 'hidden',
+            }}
+        >
+            Home
+        </div>
+    )
+}
